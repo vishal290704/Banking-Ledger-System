67ce2b9ceacec796509a47b8f42072f0d4e14e17
Vishal290704 <vsjee2002@gmail.com>
2026-06-09 21:25:03 +0530

    Checking user email whether it is already existed or not

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -4,8 +4,18 @@ const userModel = require("../models/user.model")
 
 // -User register controller
 // -POST /api/auth/register
-function userRegisterController(req, res){
+async function userRegisterController(req, res){
     const {email, password, name} = req.body
+    const isExists = await userModel.findOne({
+        email:email
+    })
+
+    if(isExists){
+        return res.status(422).json({
+            message:"User already exists with email",
+            status:"failed"
+        })
+    }
 }
 
 module.exports = {
