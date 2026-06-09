9246c903662fa8e39ed6583d5f938105568304ce
Vishal290704 <vsjee2002@gmail.com>
2026-06-09 21:19:21 +0530

    Controller and routes folder for endpoint api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require("express")
 
+
+const authRouter = require("./routes/auth.routes")
 const app = express()
+
+app.use(express.json())
+
+app.use("/api/auth", authRouter )
 
 module.exports = app
