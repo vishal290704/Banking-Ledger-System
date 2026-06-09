556c972d33426daa015f2f819614db942ef440b3
Vishal290704 <vsjee2002@gmail.com>
2026-06-09 23:24:23 +0530

    Register Api checking on postman and its working and fixing auth.controller.js(removing next)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -25,13 +25,13 @@ const userSchema = new mongoose.Schema({
     timestamps:true
 })
 
-userSchema.pre("save", async function(next){
+userSchema.pre("save", async function(){
     if(!this.isModified("password")){
-        return next()
+        return 
     }
     const hash = await bcrypt.hash(this.password, 10)
     this.password = hash
-    return next()
+    return 
 })
 
 userSchema.methods.comparePassword = async function(password){
