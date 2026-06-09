2268e1cb36d727acfa8f08a5653575d4c5710f1e
Vishal290704 <vsjee2002@gmail.com>
2026-06-09 20:39:16 +0530

    email, name and password schema is created for input from user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,6 +5,17 @@ const userSchema = mongoose.Schema({
         type:String,
         required:[true, "Email is required for creating a user"],
         trim:true,
-        lowercase:true
+        lowercase:true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/  , "Invalid e-mail address"],
+        unique:[true, "Email already exists"]
+    },
+    name:{
+        type:String,
+        required:[true, "Name is required for creating an account"],
+    },
+    password:{
+        type:String,
+        required:[true, "Password is required for creating an account"],
+        minlength: [6, "Password should contain more than 6 characters"]
     }
 })
