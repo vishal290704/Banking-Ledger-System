628ffdb0c0b0e86dc2c1a3bc20767062a3e53e62
Vishal290704 <vsjee2002@gmail.com>
2026-06-10 12:05:51 +0530

    Login api creation and userLoginController codes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,7 +3,15 @@ const authController = require("../controller/auth.controller")
 
 const router = express.Router()
 
-// POST: - /api/auth/register
+/* POST: - /api/auth/register */
 router.post("/register", authController.userRegisterController)
 
+/* POST: - /api/auth/login */
+router.post("/login", authController.userLoginController)
+
 module.exports = router
+
+
+
+
+//saare api ka end point auth.routes me create krenge
