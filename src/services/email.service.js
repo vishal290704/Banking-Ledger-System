40f4188d23103986416ec9c4aa41fa530af9a271
Vishal290704 <vsjee2002@gmail.com>
2026-06-13 20:33:50 +0530

    Email functionality and testing using postman

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -20,4 +20,34 @@ transporter.verify((error, success) => {
   }
 });
 
-module.exports = transporter;
+// Function to send email
+const sendEmail = async (to, subject, text, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Banking Ledger" <${process.env.EMAIL_USER}>`, // sender address
+      to, // list of receivers
+      subject, // Subject line
+      text, // plain text body
+      html, // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
+
+async function sendRegistrationEmail(userEmail, name){
+    const subject = "Welcome to Banking ledger"
+    const text = `Hello ${name}, \n\n Thank You for registartion at Banking-Transaction-System.
+    We're excited to have you on board!\n\nBest Regards,\nThe Banking Ledger System team.
+    `;
+    const html = `<p>Helllo ${name}, </p><p>Thank You for registartion at Banking-Transaction-System. 
+    We're excited to have you on board!</p><p>Best Regards,<br> The Banking Ledger System Team</p>`;
+    
+    await sendEmail(userEmail, subject, text, html);
+}
+module.exports = {
+    sendRegistrationEmail
+};
