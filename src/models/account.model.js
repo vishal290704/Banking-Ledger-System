aac4870f10cea514ba42b70d1b22aba4b7e14725
Vishal290704 <vsjee2002@gmail.com>
2026-06-13 23:00:47 +0530

    Account model for user bank details

---
+++ src/models/account.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const accountSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:[true, "Account must be associated with a user"],
+        index:true
+    },
+    status:{
+        enum:{
+            values:["ACTIVE", "FROZEN", "CLOSED"],
+            message:"Status can be either ACTIVE, FORZEN or CLOSED"
+        }
+    },
+    currency:{
+        type:String,
+        require:[true, "Currency is required for creating an account"],
+        default:"INR"
+    },
+},{
+    timestamps:true
+})
+
+accountSchema.index({user:1, status:1})
+
+const accountModel = mongoose.model("account", accountSchema)
+module.exports = accountModel
