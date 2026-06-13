ad3443d8dc5adcff094269833710866f227c816c
Vishal290704 <vsjee2002@gmail.com>
2026-06-13 23:16:05 +0530

    Middlware that checks user

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 
-const authRouter = require("./routes/auth.routes")
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 
+//Routes required
+const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+
+//Use Routes
 app.use("/api/auth", authRouter )
+app.use("/api/account", accountRouter)
 
 module.exports = app
---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,6 @@
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+
+async function authMiddleware(req, res, next){
+    
+}
