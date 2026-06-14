3e0cdca4f420a97da02dd92f19f25bb8edc9249a
Vishal290704 <vsjee2002@gmail.com>
2026-06-14 21:37:48 +0530

    Token checking in cookies and headers

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,5 +2,10 @@ const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
 
 async function authMiddleware(req, res, next){
-    
+     const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
+     if(!token){
+        return res.status(401).json({
+            message:"Unauthorized access, token is missing"
+        })
+     }
 }
