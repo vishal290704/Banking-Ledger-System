c9e49c375db37e009033ec94536e9621b7790212
Vishal290704 <vsjee2002@gmail.com>
2026-06-14 21:51:49 +0530

    Post api for new bank account for authenticated user

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -21,3 +21,7 @@ async function authMiddleware(req, res, next){
         })
      }
 }
+
+module.exports = {
+    authMiddleware
+}
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,6 +1,11 @@
 const express = require("express")
+const authMiddleware = require("../middleware/auth.middleware")
 const router = express.Router()
 
-
+/**
+ * -POST /api/accounts/create 
+ * -Create a new bank account for the authenticated user
+ */
+    router.post("/", authMiddleware.authMiddleware)
 
 module.exports = router
