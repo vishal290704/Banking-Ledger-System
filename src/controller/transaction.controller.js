a4baad044103d0da1686ca78d6ecf4d722fe278f
Vishal290704 <vsjee2002@gmail.com>
2026-06-17 22:05:39 +0530

    Validating idempotency key using status

--- src/controller/transaction.controller.js
+++ src/controller/transaction.controller.js
@@ -1,10 +1,60 @@
+const transactionModel = require("../models/transaction.model")
+const ledgerModel = require("../models/ledger.model")
+const accountModel = require("../models/account.model")
+const emailService = require("../services/email.service")
+
 
 async function createTransaction(req, res){
+
+    /**
+     * -Validate request (fromAccount)
+     */
     const { fromAccount, toAccount, amount, idempotencyKey } = req.body
 
-    id(!fromAccount || !toAccount || !amount || !idempotencyKey){
-        res.status(400).json({
+    if(!fromAccount || !toAccount || !amount || !idempotencyKey){
+        return res.status(400).json({
             message: "fromAccount, toAccount, amount and idempotencyKey are required"
         })
+    }
+
+    const fromUserAccount = await accountModel.findOne({
+        _id:fromAccount,
+    })
+
+    const toUserAccount = await accountModel.findOne({
+        _id: toAccount,
+    })
+
+    if(!fromUserAccount || !toUserAccount){
+        return res.status(400).json({
+            message:"Invalid fromAccount or toAccount"
+        })
+    }
+
+    /**
+     * -Validate Idempotency keys
+     */
+
+    const isTransactionAlreadyExists = await transactionModel.findOne({
+        idempotencyKey:idempotencyKey
+    })
+
+    if(isTransactionAlreadyExists){
+        if(isTransactionAlreadyExists=="COMPLETED"){
+            res.status(200).json({
+                message:"Transaction already processed",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+        if(isTransactionAlreadyExists=="PENDING"){
+            res.status(200).json({
+                message:"Transaction is still processing"
+            })
+        }
+        if(isTransactionAlreadyExists=="FAILED"){
+            res.status(500).json({
+                message:"Transaction processing failed, please retry"
+            })
+        }
     }
 }
